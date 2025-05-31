1d04d63ac1db1cee6a5b31a3497ac76fc45a7db2
phxgg <45177754+phxgg@users.noreply.github.com>
2025-05-31 16:53:55 +0300

    feat: keep a list of banned users (#22)
    * chore: bump deps
    
    * feat: keep a list of banned users

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,29 @@
+// 889686709846032414
+
+const { Events } = require("discord.js");
+
+const {
+  banned_user_ids
+} = require('../../config.json');
+
+module.exports = {
+  name: Events.GuildMemberAdd,
+  once: false,
+  /**
+   * @param {import('discord.js').GuildMember} member
+   */
+  async execute(member) {
+    const bannedUserIds = banned_user_ids || [];
+    if (bannedUserIds.includes(member.id)) {
+      return member.ban({
+        reason: "Banned user tried to join the server",
+      })
+        .then(() => {
+          console.log(`Banned ${member.user.username} for being a banned user.`);
+        })
+        .catch((err) => {
+          console.error(`Failed to ban ${member.user.username}:`, err);
+        });
+    }
+  }
+}
